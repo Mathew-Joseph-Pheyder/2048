ec8819a93614e51b689e6ddb1569a8937afe1862
Mathew Joseph <32276597+Mathew-Joseph-Pheyder@users.noreply.github.com>
2018-03-19 13:07:34 -0500

    Add files via upload

---
+++ 2048 Ripoff/src/Box.java
@@ -0,0 +1,16 @@
+import mayflower.*;
+
+public class Box extends Actor
+{
+    private int value =2;
+    public Box()
+    {
+
+    }
+
+
+    public void act()
+    {
+
+    }
+}
---
+++ 2048 Ripoff/src/Runner.java
@@ -0,0 +1,19 @@
+import mayflower.*;
+
+public class Runner extends Mayflower
+{
+    public void init()
+    {
+
+    }
+
+    public Runner()
+    {
+        super("2048",400,636);
+    }
+
+    public static void main(String[] args)
+    {
+        Runner runner = new Runner();
+    }
+}
