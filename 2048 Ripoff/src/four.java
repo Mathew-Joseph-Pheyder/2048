1c7189a61205bca21fd956e2bf59f0e10a5e6b32
vandanchavda <vandanchavda@gmail.com>
2018-03-19 13:54:18 -0500

    boxes

---
+++ 2048 Ripoff/src/four.java
@@ -0,0 +1,16 @@
+import mayflower.Actor;
+import mayflower.Keyboard;
+import mayflower.Mayflower;
+
+public class four extends Actor
+{
+    public four()
+    {
+        setImage("img/wall.png");
+    }
+
+    public void act()
+    {
+
+    }
+}
---
+++ 2048 Ripoff/src/two.java
@@ -0,0 +1,16 @@
+import mayflower.Actor;
+import mayflower.Keyboard;
+import mayflower.Mayflower;
+
+public class two extends Actor
+{
+    public two()
+    {
+        setImage("img/wall.png");
+    }
+
+    public void act()
+    {
+
+    }
+}
