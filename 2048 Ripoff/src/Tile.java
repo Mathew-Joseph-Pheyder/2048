734dbd653fbe9c2efa040c074e9e919537e38362
Mathew-Joseph-Pheyder <mathew.joseph.pheyder@gmail.com>
2018-03-23 13:18:08 -0500

    Fixed map

--- 2048 Ripoff/src/Tile.java
+++ 2048 Ripoff/src/Tile.java
@@ -1,105 +1,67 @@
 import mayflower.*;
-public class Tile extends Actor{
+public class Tile extends Actor {
     private int value;
-    public Tile(int num){
+
+    public Tile(int num) {
         value = num;
-        if(num == 2){
+        if (num == 2) {
             System.out.println("2 added");
             setImage("rsrc/2tile.png");
-        }
-        else if(num == 4){
+        } else if (num == 4) {
             setImage("rsrc/4tile.png");
-        }
-        else if(num == 8){
+        } else if (num == 8) {
             setImage("rsrc/8tile.png");
-        }
-        else if(num == 16){
+        } else if (num == 16) {
             setImage("rsrc/16tile.png");
-        }
-        else if(num == 32){
+        } else if (num == 32) {
             setImage("rsrc/32tile.png");
-        }
-        else if(num == 64){
+        } else if (num == 64) {
             setImage("rsrc/64tile.png");
-        }
-        else if(num == 128){
+        } else if (num == 128) {
             setImage("rsrc/128tile.png");
-        }
-        else if(num == 256){
+        } else if (num == 256) {
             setImage("rsrc/256tile.png");
-        }
-        else if(num == 512){
+        } else if (num == 512) {
             setImage("rsrc/512tile.png");
-        }
-        else if(num == 1024){
+        } else if (num == 1024) {
             setImage("rsrc/1024tile.png");
         }
     }
-    public int getValue(){
+
+    public int getValue() {
         return value;
     }
+
     public void act() {
-<<<<<<< HEAD
-        if(Mayflower.isKeyPressed(Keyboard.KEY_UP) && !Mayflower.isKeyDown(Keyboard.KEY_DOWN) && !Mayflower.isKeyDown(Keyboard.KEY_LEFT) && !Mayflower.isKeyDown(Keyboard.KEY_RIGHT))
-        {
-            while((getY() > 65))
-            {
-                movementDelay.set(10000);
-                if(movementDelay.isDone())
-                    setLocation(getX(), getY() - 65);
+
+        if (Mayflower.isKeyPressed(Keyboard.KEY_UP) && (!Mayflower.isKeyDown(Keyboard.KEY_DOWN) && !Mayflower.isKeyDown(Keyboard.KEY_LEFT) && !Mayflower.isKeyDown(Keyboard.KEY_RIGHT))) {
+            while ((getY() > 65)) {
+                setLocation(getX(), getY() - 65);
             }
-=======
->>>>>>> 3dbcb605369857370c736cb83fa572e45a98575d
 
-        if(Mayflower.isKeyPressed(Keyboard.KEY_UP))
-        {
-            setLocation(getX(),getY()-65);
-        }
-        if(Mayflower.isKeyPressed(Keyboard.KEY_DOWN))
-        {
-<<<<<<< HEAD
-            while((getY() < 288))
-            {
+            }
+            if (Mayflower.isKeyPressed(Keyboard.KEY_DOWN) && (!Mayflower.isKeyDown(Keyboard.KEY_UP) && !Mayflower.isKeyDown(Keyboard.KEY_LEFT) && !Mayflower.isKeyDown(Keyboard.KEY_RIGHT))) {
 
-                movementDelay.set(10000);
-                if(movementDelay.isDone())
+                while ((getY() < 132)) {
                     setLocation(getX(), getY() + 65);
+                }
+
+            }
+            if (Mayflower.isKeyPressed(Keyboard.KEY_LEFT) && (!Mayflower.isKeyDown(Keyboard.KEY_UP) && !Mayflower.isKeyDown(Keyboard.KEY_DOWN) && !Mayflower.isKeyDown(Keyboard.KEY_RIGHT))) {
+                while ((getX() > 65)) {
+                    setLocation(getX() - 65, getY());
+                }
+
+            }
+            if (Mayflower.isKeyPressed(Keyboard.KEY_RIGHT)&& (!Mayflower.isKeyDown(Keyboard.KEY_UP) && !Mayflower.isKeyDown(Keyboard.KEY_DOWN) && !Mayflower.isKeyDown(Keyboard.KEY_LEFT))) {
+                while ((getX() < 132)) {
+
+                    setLocation(getX() + 65, getY());
+                }
+
             }
 
-=======
-            setLocation(getX(),getY()+65);
->>>>>>> 3dbcb605369857370c736cb83fa572e45a98575d
+
         }
-        if(Mayflower.isKeyPressed(Keyboard.KEY_LEFT))
-        {
-<<<<<<< HEAD
-            while((getX() > 65))
-            {
-
-                movementDelay.set(10000);
-                if(movementDelay.isDone())
-                    setLocation(getX()-65,getY());
-            }
-
-=======
-            setLocation(getX()-65,getY());
->>>>>>> 3dbcb605369857370c736cb83fa572e45a98575d
-        }
-        if(Mayflower.isKeyPressed(Keyboard.KEY_RIGHT))
-        {
-<<<<<<< HEAD
-            while((getX() < 288))
-            {
-
-                movementDelay.set(10000);
-                if(movementDelay.isDone())
-                    setLocation(getX()+65,getY());
-            }
-=======
-            setLocation(getX()+65,getY());
->>>>>>> 3dbcb605369857370c736cb83fa572e45a98575d
-        }
-
-
     }
-}
+
